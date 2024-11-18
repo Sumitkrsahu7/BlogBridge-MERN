94c5b79ec1f225d94d92bdb30b1aa460caa85404
Sumit Kumar <sumitkrsahu7@gmail.com>
2024-11-18 22:41:25 +0530

    Updated the hosted backend link/ code

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://blogbridge-frontend.onrender.com/login",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
